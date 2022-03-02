11a5a47c9d0cf30410deb48ae6d62598a631cb28
Wojciech Mamys <woj.mam@gmail.com>
2022-03-02 21:39:28 +0100

    Adding get gold price functionality

--- 2_Exchange/challenge_2.py
+++ 2_Exchange/challenge_2.py
@@ -7,5 +7,17 @@ User can use this application to:
 """
 
 
+import requests
+import json
+
+
+def get_gold_price():
+    response_API = requests.get('http://api.nbp.pl/api/cenyzlota?format=json')
+    data = response_API.text
+    parse_json = json.loads(data)
+    print("Data:", parse_json[0]['data'])
+    print("Cena:", parse_json[0]['cena'])
+
+
 if __name__ == "__main__":
-    pass
+    get_gold_price()

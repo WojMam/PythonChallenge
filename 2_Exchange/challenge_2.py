18282a219cceabedb2cf7d570cb323ffa4b45ca2
Wojciech Mamys <woj.mam@gmail.com>
2022-03-02 19:41:06 +0100

    Initial commit for second challenge

---
+++ 2_Exchange/challenge_2.py
@@ -0,0 +1,11 @@
+"""
+Console Application: Exchange
+
+User can use this application to:
+    - Get the Exchange information from the banking API
+    - Calculate the exchange rates
+"""
+
+
+if __name__ == "__main__":
+    pass

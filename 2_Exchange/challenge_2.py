36f7913d3aa4c55e2d4a6d8dd4e47f2b8fe2382c
Wojciech Mamys <woj.mam@gmail.com>
2022-03-03 22:09:49 +0100

    Adding getting currency by its code function

--- 2_Exchange/challenge_2.py
+++ 2_Exchange/challenge_2.py
@@ -12,12 +12,29 @@ import json
 
 
 def get_gold_price():
-    response_API = requests.get('http://api.nbp.pl/api/cenyzlota?format=json')
-    data = response_API.text
+    response = requests.get('http://api.nbp.pl/api/cenyzlota?format=json')
+    data = response.text
     parse_json = json.loads(data)
-    print("Data:", parse_json[0]['data'])
-    print("Cena:", parse_json[0]['cena'])
+    return parse_json[0]['cena']
+
+
+def get_all_exchange_rates():
+    response = requests.get('http://api.nbp.pl/api/exchangerates/tables/a?format=json')
+    data = response.text
+    parse_json = json.loads(data)
+    return parse_json[0]['rates']
+
+
+def get_given_currency_by_code(code):
+    for currency in get_all_exchange_rates():
+        if currency['code'] == code:
+            return currency
+
+
+def get_given_currency_rate_by_code(code):
+    return get_given_currency_by_code(code)['mid']
 
 
 if __name__ == "__main__":
-    get_gold_price()
+    print(get_given_currency_by_code('EUR'))
+    print(get_given_currency_rate_by_code('EUR'))

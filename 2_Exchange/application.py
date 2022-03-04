13d4f93808a894f13931d15be1c1e0b60e8c1538
Wojciech Mamys <woj.mam@gmail.com>
2022-03-04 20:35:03 +0100

    Adding basic user actions

--- 2_Exchange/application.py
+++ 2_Exchange/application.py
@@ -10,17 +10,24 @@ from tableA import *
 from utils import *
 
 
-def choose_action():
-    pass
+def choose_action(user_input):
+    if user_input == 'table':
+        print(utils.pretty_print_json(table_a.get_all_exchange_rates()))
+    elif user_input == 'calculate':
+        print("I will do the calculation next time, ok?")
+    else:
+        utils.show_help_information()
 
 
 def exchange_calculator():
-    utils.show_welcome_msg()
+    show_welcome_msg()
     while utils.APPLICATION_STATE:
-        input(f"What you want to do?")
-        input(f"Input: ")
-        utils.valid_user_input()
-        choose_action()
+        print("What you want to do?")
+        user_input = input(f"Input: ")
+        if utils.valid_user_input(user_input):
+            choose_action(user_input)
+        else:
+            print("Sorry, we don't support this action. Please try again. Type 'help' to list the possible actions.")
 
 
 if __name__ == "__main__":
--- 2_Exchange/utils.py
+++ 2_Exchange/utils.py
@@ -1,14 +1,42 @@
+from rich.table import Table
+from rich.console import Console
+
+
+def show_welcome_msg():
+    print("Welcome")
+
+
 class Utils:
     APPLICATION_STATE = True
+    POSSIBLE_ACTIONS = ['table', 'calculate', 'help']
+
+    console = Console()
 
     def valid_user_input(self, user_input):
-        pass
-
-    def show_welcome_msg(self):
-        pass
+        if user_input in self.POSSIBLE_ACTIONS:
+            return True
+        else:
+            return False
 
     def show_help_information(self):
-        pass
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Commands", style="dim", width=12)
+        table.add_column("Description")
+        table.add_row(
+            "table", "Show whole Table A."
+        )
+        table.add_row(
+            "calculate", "Go to calculate menu."
+        )
+        table.add_row(
+            "help", "Show this beautiful table."
+        )
+
+        self.console.print(table)
+        print("Possible Actions: table, calculate, help")
 
     def close_application(self):
         self.APPLICATION_STATE = False
+
+    def pretty_print_json(self, json_object, crop=False):
+        self.console.print(json_object)
